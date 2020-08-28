6e35a4ba07f5924404b04dba5f2152699a8a94e0
Benjamin Kim <benkim96@gmail.com>
2020-08-28 16:22:42 -0700

    ch2 scaffold microposts

--- ch2/db/schema.rb
+++ ch2/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_224341) do
+ActiveRecord::Schema.define(version: 2020_08_28_232201) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
